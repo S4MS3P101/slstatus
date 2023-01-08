ded31ca42583e840046af3f34ceccd8f19f9f218
Sreekuttan E R <problem.dog@protonmail.com>
2023-01-08 16:04:21 +0530

    wifi status added

--- config.def.h
+++ config.def.h
@@ -66,7 +66,8 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format          argument */
 	{ run_command, " [ %s] ",	 "wpctl get-volume @DEFAULT_AUDIO_SINK@" },
-	{ battery_perc, "[ %s%%]",	 "BAT0" },
+	{ wifi_perc, "[ %s] ",	 "wlan0" },
+	{ battery_perc, "[  %s%%]",	 "BAT0" },
 	{ datetime, "   %s ",	 "%a, %d/%m/%Y %R" },
 
 };
